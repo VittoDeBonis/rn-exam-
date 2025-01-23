d7714d5ae58f05e6dd8fff41ace8ef69ede656ca
VittiLaTrap <debonisvittorio6@gmail.com>
2025-01-23 11:56:46 +0100

    Update
    I modified the TabNavigator

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,34 +1,16 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import ExploreTabScreen from "../screens/ExploreTab";
+import ExploreTabScreen from "../screens/ExploreTab"; 
 import FavoriteTabScreen from "../screens/FavoriteTab";
-import { TabParamList } from "./types";
+import { TabParamList } from "./types"; 
 
-const Tab = createBottomTabNavigator<TabParamList>();
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="FirstTab" 
-      screenOptions={{
-        tabBarActiveTintColor: "#007AFF",
-        tabBarInactiveTintColor: "gray",
-      }}
-    >
-      <Tab.Screen 
-        name="FirstTab" 
-        component={ExploreTabScreen}
-        options={{
-          title: "Explore",
-        }}
-      />
-      <Tab.Screen 
-        name="SecondTab" 
-        component={FavoriteTabScreen}
-        options={{
-          title: "Favorites",
-        }}
-      />
+    <Tab.Navigator>
+      <Tab.Screen name="ExploreTab" component={ExploreTabScreen} /> 
+      <Tab.Screen name="FavoriteTab" component={FavoriteTabScreen} />
     </Tab.Navigator>
   );
 };
